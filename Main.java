8c95c5578026704a738227447275cd89dc4ae00a
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2020-04-03 12:33:32 +0900

    remove generated files

--- Main.java
+++ Main.java
@@ -5,6 +5,16 @@ public class Main {
 
 		o = new Object();
 		System.out.println(o); System.out.println(o);
+		
+		//comment
+		
+		for (int i = 0; i < 10; i++) {
+			continue;
+		}
+
+		while(args.length > 1) {
+			System.out.println(o);
+		}
 	}
 
 }
